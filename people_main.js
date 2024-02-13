c79bdec12a28c21a964d32c1ff740a6f0e88c686
igorolhovskisprintit <76967096+igorolhovskisprintit@users.noreply.github.com>
2024-02-13 14:45:27 +0200

    Update people_main.js

--- people_main.js
+++ people_main.js
@@ -1,1 +1,24 @@
+$(window).on("load", function() {
 
+    $('.accordion > .card > a.card-header.collapsed').on('click', function (e) {
+        window.history.pushState("", "", $(this).attr("data-bs-target"));
+    });
+    
+    var thehash = window.location.hash;
+    thehash = thehash.replace(/[^a-zA-Z0-9_-]/g, '');
+    
+    if(thehash) {
+
+        var thehashele = $("#"+thehash).prev();
+
+        if(thehashele.length) {
+            setTimeout(function() {
+                thehashele.next().attr("class", "collapse show");
+            }, 1000);
+        }
+    }
+    
+    $("#cookies-consent-all").on("click touch", function() {
+        $("#cookies-consent-all").attr("href", "#");
+    });
+})
