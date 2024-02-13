995510062731599c11d4e0cdb07c82ed481cdf2a
igorolhovskisprintit <76967096+igorolhovskisprintit@users.noreply.github.com>
2024-02-13 09:29:23 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+$(window).on("load", function() {
+
+    var finHyph = new FinnishHyphenator();
+    var elements = $("h1 > font");
+    finHyph.hyphenateElements(elements);
+
+    $('.accordion > .card > a.card-header.collapsed').on('click', function (e) {
+        window.history.pushState("", "", $(this).attr("data-bs-target"));
+    });
+    
+    var thehash = window.location.hash;
+    thehash = thehash.replace(/[^a-zA-Z0-9_-]/g, '');
+    
+    if(thehash) {
+
+        var thehashele = $("#"+thehash).prev();
+
+        if(thehashele.length) {
+            setTimeout(function() {
+                thehashele.next().attr("class", "collapse show");
+            }, 1000);
+        }
+    }
+})
