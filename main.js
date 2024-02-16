6c19f770d90c79ceb013c69a1bfeae41e55790a0
igorolhovskisprintit <76967096+igorolhovskisprintit@users.noreply.github.com>
2024-02-16 10:39:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,32 @@ $(window).on("load", function() {
         }
     }
     
-    $("#cookies-consent-all").on("click touch", function() {
-        $("#cookies-consent-all").attr("href", "#");
-    });
 })
+
+$(document).ready(function() {
+    var pathname = window.location.pathname;
+    $("#website_cookies_bar a").each(function() {
+        if($(this).attr("href") == pathname && $(this).attr("href").length > 2) {
+            $("#website_cookies_bar").hide();
+            $("body").attr("style", "").removeClass("modal-open");
+            $("#wrapwrap").css("overflow", "auto");
+            setTimeout(function() {
+                $("body").attr("style", "overflow:auto;").removeClass("modal-open");
+                $("#wrapwrap").css("overflow", "auto");
+            }, 1500);
+        }
+        $(this).on("click touch mouseover focus", function() {
+            var arr = $(this).attr("href").split('?');
+            if(arr !== undefined) {
+                arr.pop();
+                if(arr[0] !== undefined) {
+                    url = arr[0].replace(/^.*\/\/[^\/]+/, '');
+                    if(url == "/") {
+                        url = "#";
+                    }
+                    $(this).attr("href", url);
+                }
+            }
+        });
+    });
+});
